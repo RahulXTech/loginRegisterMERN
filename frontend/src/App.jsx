29d35d029f44fb4195e902e5794ba5d759a10393
RahulXTech <rahul.code2024@gmail.com>
2026-02-18 23:58:20 +0530

    Initial commit for authentication frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Example from './componets/Navbar'
+import Navbar from './componets/Navbar'
+
+export default function App() {
+  return (
+    <div>
+       <Navbar/>
+    </div>
+  )
+}

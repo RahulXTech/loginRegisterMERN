ac001419f940c918cc577ba0f60ef4b80eb6568d
RahulXTech <rahul.code2024@gmail.com>
2026-03-28 12:08:16 +0530

    Backend cors packege issue fix

--- MGI_CampusNotes/backend/src/app.js
+++ MGI_CampusNotes/backend/src/app.js
@@ -2,13 +2,17 @@ const express = require("express")
 const cookieParser = require("cookie-parser");
 const app = express();
 require("dotenv").config()
-const cors = require('cors')
+const cors = require("cors")
 const connectDB = require("./config/db.connect")
 const userRoutes = require("./routes/user.routes");
 const uploadNotesRoute = require("./routes/upload.routes")
 app.use(cookieParser());
 app.use(express.json())
-app.use(cors())
+
+app.use(cors({
+  origin: "http://localhost:5173", //  your frontend
+  credentials: true                //  allow cookies
+}))
 
 app.use("/api/auth", userRoutes)
 app.use("/api/notes", uploadNotesRoute)

72fc8bffcd81b4080695b1fe03ac95740904010a
RahulXTech <rahul.code2024@gmail.com>
2026-04-21 15:25:09 +0530

    navbar created with logout functionality

--- RapidRecall/frontend/src/components/Navbar.jsx
+++ RapidRecall/frontend/src/components/Navbar.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
 import {navbarStyles} from '../assets/dummyStyles'
-import { Link } from 'react-router-dom'
-function Navbar() {
+import { Link, NavLink, useNavigate } from 'react-router-dom'
+import { Award, LogIn, Menu, User, X } from 'lucide-react';
+import { useState } from 'react';
+
+function Navbar({logoSrc}) {
+    const navigate = useNavigate();
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    //LOGOUT function
+    const handleLogout = ()=>{
+        try{
+            localStorage.removeItem('authToken');
+            localStorage.clear();
+        }catch(err){
+            //ignore all the error
+        }
+        window.dispatchEvent(
+            new CustomEvent("authChanged", {detail : {User : null}})
+        );
+
+        setMenuOpen(false);
+        try{
+            navigate('/login');
+        }catch(err){
+            window.Location.href = '/login'
+        }
+    }
   return (
 
     <nav className={navbarStyles.nav}>
@@ -16,11 +42,60 @@ function Navbar() {
         <div className={navbarStyles.bubble3}></div>
 
         <div className={navbarStyles.container}>
-            <div className={navbarStyles.container}>
                 <div className={navbarStyles.logoContainer}>
-                    <Link to='/' className={navbarStyles.logoInner}>
-                    </Link>
+                    <Link to="/" className={navbarStyles.logoButton}>
+                    <div className={navbarStyles.logoInner}>
+                    <img
+                        src={
+                        logoSrc ||
+                        "https://yt3.googleusercontent.com/eD5QJD-9uS--ekQcA-kDTCu1ZO4d7d7BTKLIVH-EySZtDVw3JZcc-bHHDOMvxys92F7rD8Kgfg=s900-c-k-c0x00ffffff-no-rj"
+                        }
+                        alt="QuizMaster logo"
+                        className={navbarStyles.logoImage}
+                    />
+                    </div>
+                </Link>
+            </div>
+            <div className={navbarStyles.titleContainer}>
+                <div className={navbarStyles.titleBackground}>
+                    <h1 className={navbarStyles.titleText}>Hexagon Quiz Application</h1>
                 </div>
+            </div>
+            <div className={navbarStyles.desktopButtonsContainer}>
+                        <div className={navbarStyles.spacer}></div>
+                        <NavLink to='/result' className={navbarStyles.resultsButton}>
+                            <Award className={navbarStyles.buttonIcon} />
+                            My Result
+                        </NavLink>
+                        {loggedIn ? (
+                            <button onClick={handleLogout} className={navbarStyles.logoutButton}>
+                                <LogOut className={navbarStyles.buttonIcon}/>
+                                Logout
+                            </button>
+                        ) : (
+                            <NavLink to='/login' className={navbarStyles.loginButton} >
+                                <LogIn className={navbarStyles.buttonIcon} />
+                                Login
+                            </NavLink>
+                        )}
+            </div>
+            
+            <div className={navbarStyles.mobileMenuContainer}>
+                <button onClick={()=>setMenuOpen((s)=>!s)}>
+                    {menuOpen ? (
+                        <X className={navbarStyles.menuIcon} />
+                    ) : (
+                        <Menu className={navbarStyles.menuIcon} />
+                    )}
+                </button>
+                {menuOpen && (
+                    <div className={navbarStyles.mobileMenuPanel}>
+                        <ul className={navbarStyles.mobileMenuList}>
+                             
+                        </ul>
+                    </div>
+                )}
+
             </div>
         </div>
     </nav>

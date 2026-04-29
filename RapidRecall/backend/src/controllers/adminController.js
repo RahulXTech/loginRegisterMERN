625b3376945c1753609228319984b984f98a6eb9
RahulXTech <rahul.code2024@gmail.com>
2026-04-29 20:04:31 +0530

    Add admin authorization middleware and admin management routes

---
+++ RapidRecall/backend/src/controllers/adminController.js
@@ -0,0 +1,21 @@
+import User from "../models/User.js";
+import generateToken from "../utils/generateToken.js";
+
+
+// Get Profile
+export const getUserProfile = async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    });
+  } else {
+    res.status(404).json({
+      message: "User not found",
+    });
+  }
+};

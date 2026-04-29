2751d040f19e12f1baf356ef46d7468413c69523
RahulXTech <rahul.code2024@gmail.com>
2026-04-29 19:47:57 +0530

    Setup React frontend with Tailwind CSS and routing structure

--- RapidRecall/frontend/src/App.jsx
+++ RapidRecall/frontend/src/App.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import { Routes, Route, Router } from 'react-router-dom'
-import Home from './pages/Home'
-import Navbar from './components/Navbar'
-import Login from './components/Login'
-function App() {
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
+
+const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/nav' element={<Navbar/>} />
-      <Route path='/login' element={<Login/>} />
-    </Routes>
-  )
-}
-export default App
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
+};
+
+export default App;
